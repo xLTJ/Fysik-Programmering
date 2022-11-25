d4a8c9b7202888f15da3c7b347946c36d85cf92c
xLTJ <ltj.hjem@gmail.com>
2022-11-25 13:29:56 +0100

    good stuff

--- Fysik/sketch.js
+++ Fysik/sketch.js
@@ -2,9 +2,10 @@ let x = 200;
 let y = 200;
 let w = 50;
 let angle = 0;
-let ring1Dist = 200;
-let rotationSpeed = 0.01;
-let electronDistance = 1;
+let ringDist = 100; //standart ring distance fra centrum
+let rotationSpeed = 0.01; //siger sig selv
+let electronDistance = 2; //hvilken ring elektronen er i
+let test = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -15,23 +16,41 @@ function draw() {
   background(200);
   layout();
   rotation();
-  print("working");
+
+  test += 1;
+  print(test);
+  if (test > 500) {
+    eletronDistance += 1;
+  }
 }
 
+//midten og ringene etc.
 function layout() {
   fill(199, 0, 57);
-  ellipse(windowWidth / 2, windowHeight / 2, 100);
-  noFill();
-  ellipse(windowWidth / 2, windowHeight / 2, ring1Dist * 2);
+  ellipse(windowWidth / 2, windowHeight / 2, 50);
+  for (ring = 2; ring < 6; ring++) {
+    createRing(ring);
+  }
 }
 
+//
 function rotation() {
   fill(46, 134, 193);
   translate(windowWidth / 2, windowHeight / 2);
   rotate(angle);
   angle += rotationSpeed;
-  ellipse(electronDistance * 200, 0, 50, 50);
+  ellipse((electronDistance * ringDist) / 2, 0, 25);
 }
 
-//ok
-//no
+function createRing(Distance) {
+  noFill();
+  ellipse(windowWidth / 2, windowHeight / 2, ringDist * Distance);
+}
+
+function eletronJump() {
+  if (mouseIsPressed == true) {
+    if (mouseButton == LEFT) {
+      eletronDistance += 1;
+    }
+  }
+}

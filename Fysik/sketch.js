f5c426edab670c4ef539cedc73e156a542e142cd
StagnantBytes <artyom.shevchenko2005@gmail.com>
2022-12-01 21:35:33 +0100

    indsætte formel
    Formel for Wave Length og formel for energi

--- Fysik/sketch.js
+++ Fysik/sketch.js
@@ -11,10 +11,27 @@ let photonX;
 let photonY;
 let photonVx = 1;
 let photonVy = 1.5;
+// formler variabler
+let h = -6.63 * 10 ** -34;
+let c = 3.0 * 10 ** 8;
+let R = 1.097 * 10 ** 7;
+let nm = 1000000000;
+let wave = 0;
+let energi = 0;
+let length = 1;
+let formula1 = "##";
+let formula2 = "##";
+let E1 = -2.181933 * 10 ** -18;
+let E2 = -5.4548325 * 10 ** -19;
+let E3 = -2.42437 * 10 ** -19;
+let E4 = -1.363708125 * 10 ** -19;
+let E5 = -8.727732 * 10 ** -20;
+let E6 = -6.060925 * 10 ** -20;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(280);
+  console.log(c / (E1 - E2));
 }
 
 function draw() {
@@ -24,6 +41,7 @@ function draw() {
   textThings();
   rotation();
   eletronJump();
+  waveLength();
 }
 
 //midten og ringene etc.
@@ -127,5 +145,282 @@ function photonThing() {
 }
 
 function textThings() {
-  text("electron Distance = " + electronDistance, 100, 100);
+  /*
+  noFill();
+  noStroke();
+  stroke("red");
+  strokeWeight(2);
+  */
+  fill("red");
+  //linje til højre
+  rect(0, 0, width / 4.5, height / 300);
+  rect(0, height / 4.5, width / 4.5, height / 300);
+  //linje ned
+  rect(0, 0, width / 600, height / 4.5);
+  rect(width / 4.5, 0, width / 600, height / 4.45);
+  // tekst
+
+  fill(0);
+  text("Calculations: ", width / 300, 25);
+  text(formula1, width / 300, 50);
+  text("Energy = " + energi + " J", width / 300, 75);
+  text(formula2, width / 300, 100);
+  text("Wave Length = " + wave + " nm", width / 300, 125);
+  text("Electron number = " + electronDistance, width / 300, 150);
+}
+
+function waveLength() {
+  // key 1
+  if (keyIsDown(49)) {
+    if (electronDistance == 2) {
+      formula2 = "Formula 2 = h * ( c / (E1 - E2) ) * 1*10^9";
+
+      wave = round(h * (c / (E1 - E2)) * nm);
+
+      formula1 =
+        "Formula 1 = " +
+        "(" +
+        round(E1, 20) +
+        ")" +
+        " - " +
+        "(" +
+        round(E2, 20) +
+        ")";
+
+      energi = round(E1 - E2, 20);
+
+      console.log(wave);
+    }
+    if (electronDistance == 3) {
+      formula2 = "Formula 2 = h * ( c / (E1 - E3) ) * 1*10^9";
+      wave = round(h * (c / (E1 - E3)) * nm);
+      formula1 =
+        "Formula 1 = " +
+        "(" +
+        round(E1, 20) +
+        ")" +
+        " - " +
+        "(" +
+        round(E3, 20) +
+        ")";
+      energi = round(E1 - E3, 20);
+      console.log(wave);
+    }
+    if (electronDistance == 4) {
+      formula2 = "Formula 2 = h * ( c / (E1 - E4) ) * 1*10^9";
+      wave = round(h * (c / (E1 - E4)) * nm);
+      formula1 =
+        "Formula 1 = " +
+        "(" +
+        round(E1, 20) +
+        ")" +
+        " - " +
+        "(" +
+        round(E4, 20) +
+        ")";
+      energi = round(E1 - E4, 20);
+      console.log(wave);
+    }
+    if (electronDistance == 5) {
+      formula2 = "Formula 2 = h * ( c / (E1 - E5) ) * 1*10^9";
+      wave = round(h * (c / (E1 - E5)) * nm);
+      formula1 =
+        "Formula 1 = " +
+        "(" +
+        round(E1, 20) +
+        ")" +
+        " - " +
+        "(" +
+        round(E5, 20) +
+        ")";
+      energi = round(E1 - E5, 20);
+      console.log(wave);
+    }
+    if (electronDistance == 6) {
+      formula2 = "Formula 2 = h * ( c / (E1 - E6) ) * 1*10^9";
+      wave = round(h * (c / (E1 - E6)) * nm);
+      formula1 =
+        "Formula 1 = " +
+        "(" +
+        round(E1, 20) +
+        ")" +
+        " - " +
+        "(" +
+        round(E6, 20) +
+        ")";
+      energi = round(E1 - E6, 20);
+      console.log(wave);
+    }
+    electronDistance = 1;
+  }
+  // key 2
+  if (keyIsDown(50)) {
+    if (electronDistance == 3) {
+      formula2 = "Formula 2 = h * ( c / (E2 - E3) ) * 1*10^9";
+      wave = round(h * (c / (E2 - E3)) * nm);
+      formula1 =
+        "Formula 1 = " +
+        "(" +
+        round(E2, 20) +
+        ")" +
+        " - " +
+        "(" +
+        round(E3, 20) +
+        ")";
+      energi = round(E2 - E3, 20);
+      console.log(wave);
+    }
+    if (electronDistance == 4) {
+      formula2 = "Formula 2 = h * ( c / (E2 - E4) ) * 1*10^9";
+      wave = round(h * (c / (E2 - E4)) * nm);
+      formula1 =
+        "Formula 1 = " +
+        "(" +
+        round(E2, 20) +
+        ")" +
+        " - " +
+        "(" +
+        round(E4, 20) +
+        ")";
+      energi = round(E2 - E4, 20);
+      console.log(wave);
+    }
+    if (electronDistance == 5) {
+      formula2 = "Formula 2 = h * ( c / (E2 - E5) ) * 1*10^9";
+      wave = round(h * (c / (E2 - E5)) * nm);
+      formula1 =
+        "Formula 1 = " +
+        "(" +
+        round(E2, 20) +
+        ")" +
+        " - " +
+        "(" +
+        round(E5, 20) +
+        ")";
+      energi = round(E2 - E5, 20);
+      console.log(wave);
+    }
+    if (electronDistance == 6) {
+      formula2 = "Formula 2 = h * ( c / (E2 - E6) ) * 1*10^9";
+      wave = round(h * (c / (E2 - E6)) * nm);
+      formula1 =
+        "Formula 1 = " +
+        "(" +
+        round(E2, 20) +
+        ")" +
+        " - " +
+        "(" +
+        round(E6, 20) +
+        ")";
+      energi = round(E2 - E6, 20);
+      console.log(wave);
+    }
+    electronDistance = 2;
+  }
+  // key 3
+  if (keyIsDown(51)) {
+    if (electronDistance == 4) {
+      formula2 = "Formula 2 = h * ( c / (E3 - E4) ) * 1*10^9";
+      wave = round(h * (c / (E3 - E4)) * nm);
+      formula1 =
+        "Formula 1 = " +
+        "(" +
+        round(E3, 20) +
+        ")" +
+        " - " +
+        "(" +
+        round(E4, 20) +
+        ")";
+      energi = round(E3 - E4, 20);
+      console.log(wave);
+    }
+    if (electronDistance == 5) {
+      formula2 = "Formula 2 = h * ( c / (E3 - E5) ) * 1*10^9";
+      wave = round(h * (c / (E3 - E5)) * nm);
+      formula1 =
+        "Formula 1 = " +
+        "(" +
+        round(E3, 20) +
+        ")" +
+        " - " +
+        "(" +
+        round(E5, 20) +
+        ")";
+      energi = round(E3 - E5, 20);
+      console.log(wave);
+    }
+    if (electronDistance == 6) {
+      formula2 = "Formula 2 = h * ( c / (E3 - E6) ) * 1*10^9";
+      wave = round(h * (c / (E3 - E6)) * nm);
+      formula1 =
+        "Formula 1 = " +
+        "(" +
+        round(E3, 20) +
+        ")" +
+        " - " +
+        "(" +
+        round(E6, 20) +
+        ")";
+      energi = round(E3 - E6, 20);
+      console.log(wave);
+    }
+    electronDistance = 3;
+  }
+  // key 4
+  if (keyIsDown(52)) {
+    if (electronDistance == 5) {
+      formula2 = "Formula 2 = h * ( c / (E4 - E5) ) * 1*10^9";
+      wave = round(h * (c / (E4 - E5)) * nm);
+      formula1 =
+        "Formula 1 = " +
+        "(" +
+        round(E4, 20) +
+        ")" +
+        " - " +
+        "(" +
+        round(E5, 20) +
+        ")";
+      energi = round(E4 - E5, 20);
+      console.log(wave);
+    }
+    if (electronDistance == 6) {
+      formula2 = "Formula 2 = h * ( c / (E4 - E6) ) * 1*10^9";
+      wave = round(h * (c / (E4 - E6)) * nm);
+      formula1 =
+        "Formula 1 = " +
+        "(" +
+        round(E4, 20) +
+        ")" +
+        " - " +
+        "(" +
+        round(E6, 20) +
+        ")";
+      energi = round(E4 - E6, 20);
+      console.log(wave);
+    }
+    electronDistance = 4;
+  }
+  // key 5
+  if (keyIsDown(53)) {
+    if (electronDistance == 6) {
+      formula2 = "Formula 2 = h * ( c / (E5 - E6) ) * 1*10^9";
+      wave = round(h * (c / (E5 - E6)) * nm);
+      formula1 =
+        "Formula 1 = " +
+        "(" +
+        round(E5, 20) +
+        ")" +
+        " - " +
+        "(" +
+        round(E6, 20) +
+        ")";
+      energi = round(E5 - E6, 20);
+      console.log(wave);
+    }
+    electronDistance = 5;
+  }
+  // key 6
+  if (keyIsDown(54)) {
+    electronDistance = 6;
+  }
 }
